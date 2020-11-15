b28545e7bbf82a508f345d786ae8758921e494f7
Skrkrw <josejefloen@gmail.com>
2020-11-15 23:17:40 +0000

    Setting up static/css file and media folder

--- ms4ecommerce/settings.py
+++ ms4ecommerce/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -159,3 +160,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Adding statics files
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Tupla
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"), )
+
+# Static route for amazon services - Django Collectstatic utility
+
+
+# Media Settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
