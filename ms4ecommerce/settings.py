085248cefcec58ebb02b6b95b28172953a742948
Skrkrw <josejefloen@gmail.com>
2021-01-24 21:08:06 +0000

    Testing Heroku

--- ms4ecommerce/settings.py
+++ ms4ecommerce/settings.py
@@ -33,6 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 # True only if in development environ
 #DEBUG = 'DEVELOPMENT' in os.eviron
+DEBUG = True
 
 ALLOWED_HOSTS = ['backpocket-store.herokuapp.com', 'localhost']
 
