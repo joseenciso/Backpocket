11488ff8eeeb43ec389077c0c3bad4513b38a955
Jose <josejefloen@gmail.com>
2021-02-26 22:53:35 +0000

    town_or_city misspelled in  stripe_elements js file

--- checkout/static/checkout/scripts/stripe_elements.js
+++ checkout/static/checkout/scripts/stripe_elements.js
@@ -77,7 +77,7 @@ form.addEventListener('submit', function (ev) {
                     address: {
                         line1: $.trim(form.street_address1.value),
                         line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.city_or_town.value),
+                        city: $.trim(form.town_or_city.value),
                         country: $.trim(form.country.value),
                         state: $.trim(form.county.value),
                     }
@@ -89,7 +89,7 @@ form.addEventListener('submit', function (ev) {
                 address: {
                     line1: $.trim(form.street_address1.value),
                     line2: $.trim(form.street_address2.value),
-                    city: $.trim(form.city_or_town.value),
+                    city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
                     postal_code: $trim(form.postcode.value),
                     state: $.trim(form.county.value),
