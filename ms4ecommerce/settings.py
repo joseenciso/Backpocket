9b843ebb157206552b905c9bccb73168fba3e130
joseenciso <josejefloen@gmail.com>
2021-10-17 14:19:15 +0100

    S3 bucket name settings changed

--- ms4ecommerce/settings.py
+++ ms4ecommerce/settings.py
@@ -219,7 +219,7 @@ if 'USE_AWS' in os.environ:
         'CacheControl': 'max-age=94608000'
     }
     # AWS S3 Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'backpocket-store'
+    AWS_STORAGE_BUCKET_NAME = 'backpocket-ecommerce'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
