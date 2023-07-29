e39172a2dfb2783d4f666a5299e1f721f3e37d1a
Jose Enciso <josejefloen@gmail.com>
2023-07-29 11:15:31 +0100

    Requirements updated.

--- build_files.sh
+++ build_files.sh
@@ -4,6 +4,6 @@ python manage.py loaddata ./products/fixtures/sub_category.json
 python manage.py loaddata ./products/fixtures/article.json
 python manage.py loaddata ./products/fixtures/gender.json
 python manage.py loaddata ./products/fixtures/products.json
-pip install urllib3==1.27
+pip install 
 pip install -r requirements.txt
 python manage.py collecstatic 0
