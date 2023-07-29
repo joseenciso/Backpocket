4eab456f5d69c8f6b7442ebb6bbfccd1bad3f025
Jose Enciso <josejefloen@gmail.com>
2023-07-29 07:52:22 +0100

    build_files fixtures loadddata

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,9 @@
+
+python manage.py loaddata ./products/fixtures/category.json
+python manage.py loaddata ./products/fixtures/sub_category.json
+python manage.py loaddata ./products/fixtures/article.json
+python manage.py loaddata ./products/fixtures/gender.json
+python manage.py loaddata ./products/fixtures/products.json
 pip install urllib3==1.27
 pip install -r requirements.txt
 python manage.py collecstatic 0
