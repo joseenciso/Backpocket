ed529d7225c53ae465b95742c922943513719163
Jose Enciso <josejefloen@gmail.com>
2025-05-07 19:59:49 +0100

    Exploring settings to allow Heroku to access S3 static files

--- ms4ecommerce/settings.py
+++ ms4ecommerce/settings.py
@@ -212,10 +212,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Adding statics files
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Tupla
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Tupla
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, "staticfiles"), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
