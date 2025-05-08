a99052b13b5c3ac9eb1e25b1762db1ca93a23863
Jose Enciso <josejefloen@gmail.com>
2025-05-08 12:04:48 +0100

    Django creates S3 link but this is not getting the objects

--- ms4ecommerce/custom_storages.py
+++ ms4ecommerce/custom_storages.py
@@ -1,20 +1,20 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
-#class StaticStorage(S3Boto3Storage):
-#    location = settings.STATICFILES_LOCATION
-
-#class MediaStorage(S3Boto3Storage):
-#    location = settings.MEDIAFILES_LOCATION
-
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'public-read'
+    location = settings.STATICFILES_LOCATION
 
 class MediaStorage(S3Boto3Storage):
-    location = 'media'
-    default_acl = 'public-read'
-    file_overwrite = False
+    location = settings.MEDIAFILES_LOCATION
+
+#class StaticStorage(S3Boto3Storage):
+#    location = 'static'
+#    default_acl = 'public-read'
+
+#class MediaStorage(S3Boto3Storage):
+#    location = 'media'
+#    default_acl = 'public-read'
+#    file_overwrite = False
 
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 #DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
