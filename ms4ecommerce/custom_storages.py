3aa0a344f9dcbd4c1a1841de73cb07f2d6949042
Jose Enciso <josejefloen@gmail.com>
2025-05-08 12:28:18 +0100

    S3 settings on Django

--- ms4ecommerce/custom_storages.py
+++ ms4ecommerce/custom_storages.py
@@ -1,20 +1,20 @@
 from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
+#class StaticStorage(S3Boto3Storage):
+#    location = settings.STATICFILES_LOCATION
+#
+#class MediaStorage(S3Boto3Storage):
+#    location = settings.MEDIAFILES_LOCATION
+
 class StaticStorage(S3Boto3Storage):
-    location = settings.STATICFILES_LOCATION
+    location = 'static'
+    default_acl = 'public-read'
 
 class MediaStorage(S3Boto3Storage):
-    location = settings.MEDIAFILES_LOCATION
+    location = 'media'
+    default_acl = 'public-read'
+    file_overwrite = False
 
-#class StaticStorage(S3Boto3Storage):
-#    location = 'static'
-#    default_acl = 'public-read'
-
-#class MediaStorage(S3Boto3Storage):
-#    location = 'media'
-#    default_acl = 'public-read'
-#    file_overwrite = False
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
